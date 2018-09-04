504dd86ebcca2b36829a0893ad8f944bb3d7e228
dsmminseo <dsmminseo@gmail.com>
2018-09-04 18:50:13 +0900

    Google 지도 사용

---
+++ RetrofitExam/app/src/main/java/data/model/remote/RetrofitClient.java
@@ -0,0 +1,21 @@
+package data.model.remote;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String baseUrl) {
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
