eaaf8495e87df9feb9a1f2e5256778838c0d9cc6
choidoyeong <doyeong1209@gmail.com>
2018-09-14 13:44:43 +0900

    re

---
+++ RetrofitExam/app/src/main/java/data/model/remote/ApiUtils.java
@@ -0,0 +1,10 @@
+package data.model.remote;
+
+public class ApiUtils {
+
+    public static final String BASE_URL = "https://api.stackexchange.com/2.2/";
+
+    public static SOService getSOService() {
+        return RetrofitClient.getClient(BASE_URL).create(SOService.class);
+    }
+}
